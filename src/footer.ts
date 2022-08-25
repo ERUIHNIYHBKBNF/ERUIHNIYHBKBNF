ef22ee7e84d84b0e165478723fca573a1ce933d4
chino <839203521@qq.com>
2022-08-25 22:35:31 +0800

    fix: 调整生成时间间隔

--- src/footer.ts
+++ src/footer.ts
@@ -2,7 +2,7 @@ import { timeZone } from "./config";
 
 export async function getFooter() {
   return `
-<p align="center">此文件 <i>README</i> <b>间隔 3 小时</b>自动刷新生成！
+<p align="center">此文件 <i>README</i> <b>间隔 12 小时</b>自动刷新生成！
 </br>
 最近一次刷新于：${new Date().toLocaleString(undefined, {
     timeStyle: "medium",
