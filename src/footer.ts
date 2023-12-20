76a00283db7d619edde4527b38c4b76d3ac1d244
chino <839203521@qq.com>
2023-12-21 00:45:57 +0800

    feat: update readme

--- src/footer.ts
+++ src/footer.ts
@@ -1,15 +1,16 @@
 import { timeZone } from "./config";
 
 export async function getFooter() {
-  return `
-<p align="center">此文件 <i>README</i> <b>间隔 12 小时</b>自动刷新生成！
-</br>
-最近一次刷新于：${new Date().toLocaleString(undefined, {
-    timeStyle: "medium",
-    dateStyle: "short",
-    hour12: false,
-    timeZone,
-  })}
-</p>
-  `;
+//   return `
+// <p align="center">此文件 <i>README</i> <b>间隔 12 小时</b>自动刷新生成！
+// </br>
+// 最近一次刷新于：${new Date().toLocaleString(undefined, {
+//     timeStyle: "medium",
+//     dateStyle: "short",
+//     hour12: false,
+//     timeZone,
+//   })}
+// </p>
+//   `;
+  return '';
 }
